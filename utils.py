f8548b5ca5650df560783f132f617d8dd6bed80a
Divyansh Srivastava <divyan@microsoft.com>
2021-10-31 17:37:49 -0700

    Limit to CPU

--- utils.py
+++ utils.py
@@ -61,13 +61,13 @@ def setup_mpi_gpus():
     """
     Set CUDA_VISIBLE_DEVICES using MPI.
     """
-    available_gpus = guess_available_gpus()
+    # available_gpus = guess_available_gpus()
 
-    node_id = platform.node()
-    nodes_ordered_by_rank = MPI.COMM_WORLD.allgather(node_id)
-    processes_outranked_on_this_node = [n for n in nodes_ordered_by_rank[:MPI.COMM_WORLD.Get_rank()] if n == node_id]
-    local_rank = len(processes_outranked_on_this_node)
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(available_gpus[local_rank])
+    # node_id = platform.node()
+    # nodes_ordered_by_rank = MPI.COMM_WORLD.allgather(node_id)
+    # processes_outranked_on_this_node = [n for n in nodes_ordered_by_rank[:MPI.COMM_WORLD.Get_rank()] if n == node_id]
+    # local_rank = len(processes_outranked_on_this_node)
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
 def guess_available_cpus():
