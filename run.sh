ff1bd136dcaac0b70f4b684ad5558198bbc9f166
Shubham Chaudhary <chaudharyshubham2011@gmail.com>
2021-11-13 08:57:00 -0800

    Enable use of 2 gpus for faster training

--- run.sh
+++ run.sh
@@ -45,4 +45,4 @@
 # pip install atari-py
 
 python -m atari_py.import_roms ./content/ROM/
-python run.py
+mpiexec -n 2 python run.py
--- start_pod.sh
+++ start_pod.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-launch-scipy-ml-gpu.sh -i somil55/tf1gym:latest -P Always
+# if 2080ti is not availble try 1080ti, then 1070ti
+launch-scipy-ml-gpu.sh -g 2 -c 8 -v 2080ti -m 64  -i somil55/tf1gym:latest -P Always
