763bfb3517a14df8c6b6b720c926b759640568da
ddivyansh <ddivyansh@ddivyansh-30830.pods.ddivyansh.svc.cluster.local>
2021-11-18 21:23:05 -0800

    update runtime configuration

--- run.sh
+++ run.sh
@@ -45,4 +45,4 @@
 # pip install atari-py
 
 python -m atari_py.import_roms ./content/ROM/
-mpiexec -n 2 python run.py
+mpiexec -n 2 python run.py --env 'RiverraidNoFrameskip-v4'
--- start_pod.sh
+++ start_pod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # if 2080ti is not availble try 1080ti, then 1070ti
-launch-scipy-ml-gpu.sh -g 2 -c 8 -v 2080ti -m 64  -i somil55/tf1gym:latest -P Always
+launch-scipy-ml-gpu.sh -g 2 -c 8 -v 2080ti -m 32  -i somil55/tf1gym:latest -P Always -b
